f551ad58a0cb1aa6576d751d61085f95363da939
David Voong <david.voong@dataspartan.com>
2026-02-06 16:45:37 +0000

    update

--- src/app/signals/page.tsx
+++ src/app/signals/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
 
   useEffect(() => {
     signalActions.setSignals(mockData.map(parseSignalDTO));
-  }, [signalActions]);
+  }, []);
 
   return (
     <Container>
